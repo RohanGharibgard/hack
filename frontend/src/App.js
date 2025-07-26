4b5c410da4b0aba90f49507c2bbac3596c6d36ec
Rohan Gharibgard <rohang2449@gmail.com>
2025-07-26 00:38:37 -0700

    Yea no ones gonna read these anyways.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import io from 'socket.io-client';
 import './App.css';
+import { Camera } from "./Camera";
 
-const socket = io('http://localhost:8000');
+export const socket = io('http://localhost:8000');
+export const AppContext = createContext(null);
 
 function App() {
-  const [pictureStatus, setPictureStatus] = useState("");
-
-  useEffect(() => {
-    socket.on('connect', () => console.log('Connected:', socket.id));
-    socket.on('picture_taken', data => {
-      setPictureStatus(data.message);
-      setTimeout(() => setPictureStatus(""), 3000); // Clear status after 3 seconds
-    });
-    return () => {
-      socket.off('picture_taken');
-    };
-  }, []);
 
   return (
+    <AppContext.Provider value={{
+    }}>
     <div className="app">
-      <p>Write your code here!</p>
+      <div>
+        <h1>Fabulous4</h1>
+      </div>
+      <Camera />
     </div>
+    </AppContext.Provider>
   );
 }
 
--- pico/connections.py
+++ pico/connections.py
@@ -1,5 +1,6 @@
 from simple import MQTTClient
 import ssl
+
 from time import sleep
 
 
