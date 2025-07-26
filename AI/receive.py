cd78dde2bf3b29f2304395e4995f67241e05a7f7
Rohan Gharibgard <rohang2449@gmail.com>
2025-07-26 00:34:05 -0700

    Updated code for AI analysis.

--- AI/receive.py
+++ AI/receive.py
@@ -3,15 +3,16 @@
 import requests
 import os
 import sys
+from send_to_openai import speech_analyze_image
 
 # Get the folder where the script is located, done for you
 script_dir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(script_dir, "downloaded_image.jpg")
-
+image_filename = os.path.join(script_dir, "../frontend/src/downloaded_image.jpg")
+mp3_filename = os.path.join(script_dir, "../frontend/src/image_analysis.mp3")
 url = "http://172.20.10.6/1024x768.jpg"             # You will have to change the IP Address
 
 # Function to download the image from esp32, given to you
-def download_image():
+def download_image(filename: str):
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -22,6 +23,5 @@ def download_image():
         print("Failed to download image. Status code:", response.status_code)
 
 # TODO: Download the image and get a response from openai
-
-# TODO: How to control when to take photo?
-
+download_image(image_filename)
+speech_analyze_image(image_filename,  "Analyze the image and look for clues.", mp3_filename, "Speak like a detective.")
--- AI/send_to_openai.py
+++ AI/send_to_openai.py
@@ -1,8 +1,16 @@
-
 # TODO: Import your libaries
+import base64
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # TODO: Maybe you need a key?
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+if not OPENAI_API_KEY:
+    raise ValueError("No API key found.")
 
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 # Image encoding, code provided
 def encode_image(image_path):
@@ -12,11 +20,60 @@ def encode_image(image_path):
 
 # TODO: Sending a request and getting a response
 
+def analyze_image(image_path: str, prompt: str):
+    """
+    Analyzes an image according to the prompt and returns the analysis as a string.
+    
+    Args:
+        image_path (str): path to the image to analyze.
+        prompt (str): the prompt used to analyze the image.
+    Returns:
+        (Response): The analysis of the image.    
+    """
+    base64_image = encode_image(image_path)
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    { "type": "input_text", "text": "what's in this image?" },
+                    {
+                        "type": "input_image",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            }
+        ],
+    )
+    print(response.output_text)
+    return response
 
 
 # TODO: How do we make things audible?
-    
 
+def text_to_speech(text: str, mp3_output_path: str, speech_instructions: str):
+    """
+    Convert a text to speech
+    Args:
+        text (str): the text to transcribe.
+        mp3_output_path (str): The path to output the transcription.
+        instructions (str): The instructions used to transcribe the text.
+    """
+
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="ash",
+        input=text,
+        instructions=speech_instructions,
+    ) as response:
+        response.stream_to_file(mp3_output_path)
 
 # TODO: Can we put everything together?
+def speech_analyze_image(image_path: str, image_prompt: str, mp3_output_path: str, speech_prompt: str):
+    text_analysis = analyze_image(image_path, image_prompt).output_text
+    text_to_speech(text_analysis, mp3_output_path, speech_prompt)
 
+if __name__ == '__main__':
+    speech_analyze_image("image.jpg", "What is this an image of?", "test.mp3", "Speak like a knowlagible ")
